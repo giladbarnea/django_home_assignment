28ab632dbf6358d15fc735e3b6aa51f89284c00c
Gilad Barnea <giladbrn@gmail.com>
2020-12-16 22:44:24 +0200

    /send/ endpoint

--- djangoroku_app/urls.py
+++ djangoroku_app/urls.py
@@ -6,4 +6,5 @@ app_name = 'djangoroku_app'
 
 urlpatterns = [
     path(r'', views.index, name='index'),
+    path(r'send/', views.send_msg, name='send_msg'),
     ]
--- djangoroku_app/views.py
+++ djangoroku_app/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render, HttpResponse
 
 def index(request, *args, **kwargs):
     print(f'index(args: {args}, kwargs: {kwargs})')
-    return "Hello from index"
+    return HttpResponse(b"Hello from index")
+
+
+def send_msg(request, *args, **kwargs):
+    print(f'send_msg(args: {args}, kwargs: {kwargs})')
+    return HttpResponse(b"Hello from send_msg")
